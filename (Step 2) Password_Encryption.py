79d4174389ca92cd8ea06a17b8209f75ce014a89
Noah <56573280+Noah6544@users.noreply.github.com>
2021-07-08 17:17:20 -0400

    Making code more concise and adding descriptions.
    added some user instructions and verified that this step is working as intended because I finally came around to working on this project again.

--- (Step 2) Password_Encryption.py
+++ (Step 2) Password_Encryption.py
@@ -11,12 +11,13 @@ enter all information in variable starting with email.
 you have to actually put your real password below. You can erase it after you run this program but you must enter it directly below. this sounds like a scam when i say it like this...but i'm not advanced enough to figure out how to grab passwords. wow this even more like a scam. its not tho just trying to make this code avaliable for anyone who might want to encrypt passwords.
 """"
 actualprogrampassword = "enter here"
-Key = open("Key.txt","rb")
-key = bytes(Key.read())
-Key.close()
+Key = open("Key.txt","rb").read()
 cipher_suite = Fernet(Key)
-programpassfile = open("programpass.txt","wb")
-encoded_text = cipher_suite.encrypt(b"actualprogrampassword")
+programpassfile = open("C:\\userdirectory\\programpass.txt","wb") #insert correct file path, adding "\\" for every directory change idk y 
+encoded_text = cipher_suite.encrypt(b"actualprogrampassword") #encrypts your password entered above
 programpassfile = programpassfile.write(encoded_text) #if it doesnt work add ".bytes()" ? i think. 
+"""
+#these two lines simply decrypt your password, you can then use these for the gui automation in the next step (click and typing)
 programpassfile = cipher_suite.decrypt(programpassfile)
 programpassfile = programpassfile.decode('utf-8')
+"""
